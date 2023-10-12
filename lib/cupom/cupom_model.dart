f126b30dc93e52880554d655acf1657497e61fab
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-10-12 02:14:21 +0000

    Updating to latest FlutterFlow output.

--- lib/cupom/cupom_model.dart
+++ lib/cupom/cupom_model.dart
@@ -1,19 +1,16 @@
 import '/flutter_flow/flutter_flow_button_tabbar.dart';
-import '/flutter_flow/flutter_flow_icon_button.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
+import 'cupom_widget.dart' show CupomWidget;
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 
-class CupomModel extends FlutterFlowModel {
+class CupomModel extends FlutterFlowModel<CupomWidget> {
   ///  State fields for stateful widgets in this page.
 
   final unfocusNode = FocusNode();
-  // State field(s) for searchBar widget.
-  TextEditingController? searchBarController;
-  String? Function(BuildContext, String?)? searchBarControllerValidator;
   // State field(s) for TabBar widget.
   TabController? tabBarController;
   int get tabBarCurrentIndex =>
@@ -25,7 +22,6 @@ class CupomModel extends FlutterFlowModel {
 
   void dispose() {
     unfocusNode.dispose();
-    searchBarController?.dispose();
     tabBarController?.dispose();
   }
 
--- lib/login/login_widget.dart
+++ lib/login/login_widget.dart
@@ -79,7 +79,9 @@ class _LoginWidgetState extends State<LoginWidget>
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () => FocusScope.of(context).requestFocus(_model.unfocusNode),
+      onTap: () => _model.unfocusNode.canRequestFocus
+          ? FocusScope.of(context).requestFocus(_model.unfocusNode)
+          : FocusScope.of(context).unfocus(),
       child: Scaffold(
         key: scaffoldKey,
         backgroundColor: Colors.white,
