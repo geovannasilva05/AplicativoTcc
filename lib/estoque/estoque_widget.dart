412af1399a27aeb13ab176cd8472349a7f71e181
Diego0668 <diegolimaxd1@gmail.com>
2023-10-14 22:12:12 -0300

    update

--- lib/estoque/estoque_widget.dart
+++ lib/estoque/estoque_widget.dart
@@ -179,6 +179,7 @@ class _EstoqueWidgetState extends State<EstoqueWidget> {
                                         padding: EdgeInsetsDirectional.fromSTEB(
                                             0.0, 12.0, 4.0, 8.0),
                                         child: Text(
+                                          'Quantidade:',
                                            lista.elementAt(index).getQuantidade.toString(),
                                           textAlign: TextAlign.start,
                                           style: FlutterFlowTheme.of(context)
